0438395ace1514bb3792f012df2bda52d0572afb
DevelopRohit <rohitmohadikar897@gmail.com>
2026-03-16 13:09:20 +0530

    cors rule change

--- backend/app.py
+++ backend/app.py
@@ -21,6 +21,7 @@ def build_allowed_origins():
         "http://localhost:5173",
         "http://127.0.0.1:5173",
         "http://localhost:3000",
+        "http://paymentgetwayindia.netlify.app",
     }
 
     extra_origins = os.getenv("CORS_ORIGINS", "")
